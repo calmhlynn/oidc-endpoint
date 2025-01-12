2967f6666361b4625c8ec4ea05905e0048ea6429
Seonkyo Jung <barca105@naver.com>
2025-01-12 10:29:04 +0900

    Fix(openapi): Delete utoipa for temporary work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-pub mod api;
 pub mod logger;
 pub mod util;
 
@@ -7,35 +6,16 @@ use std::{
     net::{Ipv4Addr, SocketAddr},
 };
 
-use api::{
-    one::{self, get_one},
-    two::{self, get_two},
-};
 use axum::{middleware, routing::get, Router};
 use logger::metrics_builder::{recorder_builder, track_metrics};
 use tokio::{io, net::TcpListener};
 use util::jwt::SecurityAddon;
-use utoipa::OpenApi;
-use utoipa_swagger_ui::SwaggerUi;
-
-#[derive(OpenApi)]
-#[openapi(
-    nest(
-        (path = "/one", api = one::OneApi),
-        (path = "/two", api = two::TwoApi)
-    ),
-    modifiers(&SecurityAddon)
-)]
-struct ApiDoc;
 
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
     let metrics_handler = recorder_builder();
     let app = Router::new()
         .route("/", get(root))
-        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
-        .route("/api/v1/ones", get(get_one))
-        .route("/api/v1/twos", get(get_two))
         .route("/metrics", get(move || ready(metrics_handler.render())))
         .route_layer(middleware::from_fn(track_metrics));
     // .nest("/metrics", prometheus_router().await);
